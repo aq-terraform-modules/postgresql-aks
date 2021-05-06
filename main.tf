f14cf2cfab40483e6d5735a00906294fb7053627
Anh Quach <anh.quachquituan@halliburton.com>
2021-05-06 23:23:34 +0700

    Add stuffs

--- main.tf
+++ main.tf
@@ -3,12 +3,34 @@ resource "azurerm_resource_group" "aks-cluster-rg" {
   location  = var.location
 
   tags = {
-    Environment = "Development"
+    Environment = var.tag_env
   }
 
   lifecycle {
     ignore_changes  = [tags]
   }
+}
+
+# Log analytics for Container monitoring
+resource "azurerm_log_analytics_workspace" "aks-log-wspace" {
+  # The WorkSpace name has to be unique across the whole of azure, not just the current subscription/tenant.
+  name                = "${var.aks_name}-workspace-${random_id.log_analytics_workspace_name_suffix.dec}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.aks-cluster-rg.name
+  sku                 = var.log_analytics_workspace_sku
+}
+
+resource "azurerm_log_analytics_solution" "aks-log-solution" {
+    solution_name         = "ContainerInsights"
+    location              = var.location
+    resource_group_name   = azurerm_resource_group.aks-cluster-rg.name
+    workspace_resource_id = azurerm_log_analytics_workspace.aks-log-wspace.id
+    workspace_name        = azurerm_log_analytics_workspace.aks-log-wspace.name
+
+    plan {
+        publisher = "Microsoft"
+        product   = "OMSGallery/ContainerInsights"
+    }
 }
 
 resource "azurerm_kubernetes_cluster" "aks-cluster" {
@@ -17,21 +39,44 @@ resource "azurerm_kubernetes_cluster" "aks-cluster" {
   resource_group_name = azurerm_resource_group.aks-cluster-rg.name
   dns_prefix          = "${var.aks_name}-dns"
 
+  #  Resource group for node
+  node_resource_group = "${var.resource_group_name}-node"
+
   default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_D2_v2"
+    name                = var.nodepool_name
+    type                = var.nodepool_type
+    vm_size             = var.node_size
+    enable_auto_scaling = var.node_auto_scale
+    node_count          = ${var.node_auto_scale == false ? var.node_count : null}
+    max_count           = ${var.node_auto_scale == true ? var.node_max_count : null}
+    min_count           = ${var.node_auto_scale == true ? var.node_min_count : null}
   }
 
   identity {
     type = "SystemAssigned"
   }
 
-  #  Resource group for node
-  node_resource_group = "${var.resource_group_name}-node"
-  
+  # Network
+  network_profile {
+    load_balancer_sku = "Standard"
+    network_plugin = "kubenet"
+  }
+
+  # Addon
+  addon_profile {
+    oms_agent {
+      enabled = true
+      log_analytics_workspace_id = azurerm_log_analytics_workspace.aks-log-wspace.id
+    }
+
+    kube_dashboard {
+      enabled = true
+    }
+  }
+
+  #  Tags
   tags = {
-    Environment = "Development"
+    Environment = var.tag_env
   }
 
   lifecycle {
--- variable.tf
+++ variable.tf
@@ -10,3 +10,45 @@ variable "location" {
 variable "aks_name" {
   description = "Name for logic app"
 }
+
+variable "tag_env" {
+  description = "Environment tag"
+}
+
+variable "nodepool_name" {
+  default     = "default"
+  description = "Name for nodepool that will contain all nodes"
+}
+
+variable "nodepool_type" {
+  default     = "VirtualMachineScaleSets"
+  description = "Name for nodepool that will contain all nodes"
+}
+
+variable "node_count" {
+  default     = "1"
+  description = "Total node for nodepool"
+}
+
+variable "node_size" {
+  default     = "Standard_D2_v2"
+  description = "Size for node of nodepool"
+}
+
+variable "node_auto_scale" {
+  description = "Node autoscaling"
+}
+
+variable "node_min_count" {
+  default     = 1
+  description = "Node min count with autoscaling on"
+}
+
+variable "node_max_count" {
+  default     = 2
+  description = "Node max count with autoscaling on"
+}
+
+variable log_analytics_workspace_sku {
+    default = "PerGB2018"
+}
