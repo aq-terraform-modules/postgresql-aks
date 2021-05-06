cee4df3f73d0de32e81126b3ab4e81aa883e186f
Anh Quach <anh.quachquituan@halliburton.com>
2021-05-06 20:26:46 +0700

    Update output.tf and add k8s cluster resource

--- main.tf
+++ main.tf
@@ -6,3 +6,28 @@ resource "azurerm_resource_group" "aks-rg" {
     ignore_changes  = [tags]
   }
 }
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "${var.aks_name}-cluster"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.aks-rg.name
+  dns_prefix          = "${var.aks_name}-dns"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Development"
+  }
+
+  lifecycle {
+    ignore_changes  = [tags]
+  }
+}
